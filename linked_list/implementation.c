03f6d81d0ecd043bd65bc0c6d40ab079a063867f
Sanoj Silva <silvasanoj@gmail.com>
2020-01-12 21:51:57 +0530

    Add append, prepend, printList to linked list implementation

--- linked_list/implementation.c
+++ linked_list/implementation.c
@@ -3,22 +3,60 @@
 
 struct Node {
     int data;
-    struct Node* link;
+    struct Node* next;
 };
 
-struct Node* newNode(int data) {
-    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
-    node->data = data;
-    node->link = NULL;
-    return node;
+struct Node* start = NULL;
+
+struct Node* createNode(int value) {
+    struct Node* tmp = (struct Node*)malloc(sizeof(struct Node));
+    tmp->data = value;
+    tmp->next = NULL;
+    return tmp;
+}
+
+void prepend(struct Node* newNode) {
+    if (start == NULL) {
+        start = newNode;
+    } else {
+        struct Node* tmp = start;
+        start = newNode;
+        newNode->next = tmp;
+    }
+}
+
+void append(struct Node* newNode) {
+    if (start == NULL) {
+        start = newNode;
+    } else {
+        struct Node* tmp = start;
+        while(tmp->next != NULL) {
+            tmp = tmp->next;
+        }
+        tmp->next = newNode;
+    }
+}
+
+void printList() {
+    struct Node* tmp = start;
+    while(tmp != NULL) {
+        printf("%d->", tmp->data);
+        tmp = tmp->next;
+    }
+    printf("NULL\n");
 }
 
 int main() {
-    struct Node* first = newNode(5);
-    struct Node* second = newNode(10);
-    second->link = first;
-    printf("%d\n", first->data);
-    printf("first => %p\n", first);
-    printf("second => %p\n", second->link);
+    int i;
+
+    for (i = 0; i < 6; i++) {
+        prepend(createNode(i));
+    }
+
+
+    for (i = 1; i < 6; i++) {
+        append(createNode(i));
+    }
+    printList();
     return 0;
 }
